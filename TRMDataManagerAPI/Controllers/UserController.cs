dfc2ffaa44b45ab20b00ba9995a3aff92327e0f8
Yacine <yacine.hayat@gmail.com>
2021-04-27 02:50:18 +0200

    Ajout class library pour recuperer les données en base avec webAPI

---
+++ TRMDataManagerAPI/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TRMDLL.DataAccess;
+using TRMDLL.Models;
+
+
+namespace TRMDataManagerAPI.Controllers
+{
+    [Authorize]
+    [RoutePrefix("/api/User")]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            UserData data = new UserData();
+            
+            return data.GetUserById(userId);
+        }
+    }
+}
